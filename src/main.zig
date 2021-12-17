2f8a95eb6fc567d2b42f5ac3982ad6ef1c413d8b
Peter Lichard <peter.lichard@gmail.com>
2021-12-17 01:25:22 +0100

    Wip

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 const board = @import("board.zig");
 const utils = @import("utils.zig");
- 
+
 const n = board.sum_for_all_positions(board.number_of_moves_for_pawns);
 
 fn take_some_slice(slice: []u32) void {
@@ -19,22 +19,10 @@ pub fn main() anyerror!void {
     std.log.debug("sizeof cells: {} bytes", .{@sizeOf(@TypeOf(b.cells))});
     std.log.debug(
         \\ size of:
-        \\ Move:          {} bytes ({} bits)
+        \\ PieceMove:     {} bytes ({} bits)
         \\ Action:        {} bytes ({} bits)
-        \\ ActionMove:    {} bytes ({} bits)
-        \\ ActionCapture: {} bytes ({} bits)
-        \\ ActionPromote: {} bytes ({} bits)
         \\ Board:         {} bytes
-        , 
-        .{
-            @sizeOf(board.Move), @bitSizeOf(board.Move),
-            @sizeOf(board.Action), @bitSizeOf(board.Action),
-            @sizeOf(board.ActionMove),  @bitSizeOf(board.ActionMove),
-            @sizeOf(board.ActionCapture), @bitSizeOf(board.ActionCapture),
-            @sizeOf(board.ActionPromote), @bitSizeOf(board.ActionPromote),
-            @sizeOf(board.Board)
-        }
-    );
+    , .{ @sizeOf(board.PieceMove), @bitSizeOf(board.PieceMove), @sizeOf(board.Action), @bitSizeOf(board.Action), @sizeOf(board.Board) });
 
     std.log.debug(
         \\ 
@@ -42,23 +30,15 @@ pub fn main() anyerror!void {
         \\ KINGS      : {}
         \\ KNIGHTS    : {}
         \\ PAWNS      : {}
-        ,
-        .{
-            utils.number_of_moves_for_main_pieces(),
-            utils.number_of_moves_for_kings(),
-            utils.number_of_moves_for_knights(),
-            utils.number_of_moves_for_pawns()
-        }
-    );
+    , .{ utils.number_of_moves_for_main_pieces(), utils.number_of_moves_for_kings(), utils.number_of_moves_for_knights(), utils.number_of_moves_for_pawns() });
 
-    var slice = [_]u32{0,1,2,3,4,5,6,7,8,9,10};
+    var slice = [_]u32{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     take_some_slice(slice[4..]);
     std.log.debug("slice[5] = {}", .{slice[5]});
 
-
     std.log.debug("MAX_MOVES = {}", .{utils.MAX_MOVES});
 
-    var moves = std.ArrayList(board.Move).init(std.heap.c_allocator);
+    var moves = std.ArrayList(board.PieceMove).init(std.heap.c_allocator);
 
     try b.collect_all_moves(&moves);
 
